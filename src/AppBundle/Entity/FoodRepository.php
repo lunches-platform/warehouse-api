6723a99633c1668fd5344bb5e77f66e1e7af7759
Warlock <warlock39@gmail.com>
2016-09-16 20:27:25 +0300

    [Food] (WAPI-6) Added 'like' API param to filter foods by name with LIKE pattern

--- src/AppBundle/Entity/FoodRepository.php
+++ src/AppBundle/Entity/FoodRepository.php
@@ -9,4 +9,14 @@ use Doctrine\ORM\EntityRepository;
  */
 class FoodRepository extends EntityRepository
 {
+    /**
+     * @param string $like
+     * @return array
+     */
+    public function findByNameLike($like)
+    {
+        $dql = 'SELECT f FROM AppBundle\Entity\Food f WHERE f.name LIKE :like';
+
+        return $this->_em->createQuery($dql)->setParameter('like', '%'.$like.'%')->getResult();
+    }
 }
