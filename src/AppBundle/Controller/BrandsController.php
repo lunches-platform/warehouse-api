6c06d38a9863cff917d88c3504cca982c0990a6e
Warlock <warlock39@gmail.com>
2016-09-20 16:51:37 +0300

    [Controllers] (WAPI-21) Refactored BrandsController to follow Controller as a Service principle

--- src/AppBundle/Controller/BrandsController.php
+++ src/AppBundle/Controller/BrandsController.php
@@ -5,17 +5,30 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Brand;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 use FOS\RestBundle\Controller\Annotations\RequestParam;
 use FOS\RestBundle\Controller\Annotations\View;
-use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Request\ParamFetcher;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * Class BrandsController.
  */
-class BrandsController extends FOSRestController
+class BrandsController
 {
+    /**
+     * @var Registry
+     */
+    protected $doctrine;
+
+    /**
+     * FoodsController constructor.
+     * @param Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
     /**
      * @param Brand $brand
      * @return \Symfony\Component\HttpFoundation\Response
@@ -33,7 +46,7 @@ class BrandsController extends FOSRestController
      */
     public function getBrandsAction()
     {
-        $repo = $this->getDoctrine()->getRepository('AppBundle:Brand');
+        $repo = $this->doctrine->getRepository('AppBundle:Brand');
 
         return $repo->findAll();
     }
@@ -50,12 +63,12 @@ class BrandsController extends FOSRestController
     public function postBrandsAction(ParamFetcher $params)
     {
         $name = $params->get('name');
-        if ($this->getDoctrine()->getRepository('AppBundle:Brand')->findOneBy(['name' => $name])) {
+        if ($this->doctrine->getRepository('AppBundle:Brand')->findOneBy(['name' => $name])) {
             throw new HttpException(400, 'Brand with specified name is exist already');
         }
         $brand = new Brand($name);
 
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->doctrine->getManager();
         $em->persist($brand);
         $em->flush();
 
